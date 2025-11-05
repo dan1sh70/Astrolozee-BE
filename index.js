6d435bbcc110400179b7edeb493b3090ca39a645
Danish shafiq <67628974+dan1sh70@users.noreply.github.com>
2025-11-05 23:23:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS config
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://www.astrolozee.com",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
